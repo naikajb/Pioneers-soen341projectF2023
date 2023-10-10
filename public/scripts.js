204f5c2a48a87f4eec8db3692dafebfdbdbd3fd6
mbedair <mbedairgis@gmail.com>
2023-10-10 12:39:20 -0400

    On branch Mohamed's-branch Your branch is up to date with 'origin/Mohamed's-branch'.
    Changes to be committed:
    	modified:   app.js
    	new file:   public/scripts.js
    	new file:   public/styles.css
    	new file:   views/dummyData.js
    	modified:   views/layout.ejs
    	new file:   views/listing.ejs

---
+++ public/scripts.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+  }
+  
+  function closePopup() {
+    document.getElementById("popup").style.display = "none";
+  }
+  
+  
---
+++ views/dummyData.js
@@ -0,0 +1,25 @@
+const dummyListings = [
+    {
+      id: 1,
+      address: '1234 Elm St',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      price: 500000,
+      bedrooms: 3,
+      bathrooms: 2,
+      amenities: ['Swimming Pool', 'Garden', 'Garage'],
+      broker: { name: 'John Doe', contact: '123-456-7890' }
+    },
+    {
+      id: 2,
+      address: '5678 Oak St',
+      description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      price: 600000,
+      bedrooms: 4,
+      bathrooms: 3,
+      amenities: ['Gym', 'Fireplace', 'Balcony'],
+      broker: { name: 'Jane Smith', contact: '987-654-3210' }
+    }
+  ];
+  
+  module.exports = dummyListings;
+  
