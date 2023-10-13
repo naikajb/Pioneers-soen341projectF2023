9516429ff831880d2a4378a224b727aac870a0b4
naikajb <naika1492@gmail.com>
2023-10-13 15:17:43 -0400

    navbar for website w/out size responsiveness

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,15 +1,26 @@
-
+import logo from './logo.png';
+import navIcon from './navigation-bar.png';
 function Navbar(){
 
     return (
-        <div> 
-            <nav>
-            <a href = "index.js" class = "navbar-logo"> 
-            <img src = "/logo.png" alt = "logo" width = "50" height = "50"/>
-            </a>
-
-            </nav>
-        </div>
+        
+        <nav className='Navbar'>
+            <div>
+            <img class = "Navbar-logo" src = {logo} alt = "logo" width = "100%" height = "100%"/> 
+            </div>
+            <p className='companyName'> 
+                <h1>Company Name</h1>
+            </p>    
+            <ul>
+                <li className='Navlinks'>Home</li>
+                <li className='Navlinks'>Menu</li>
+                <li className='Navlinks'>Order</li>
+                <li className='Navlinks'>Contact</li>
+                <img class = "nav-icon" src = {navIcon} alt = "logo" width = "10%" height = "50%"/>
+                
+            </ul>
+        </nav>
+        
     )
 }
 export default Navbar;
