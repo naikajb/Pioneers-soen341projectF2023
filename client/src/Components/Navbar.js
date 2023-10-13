b05bbc06f6dabf12a47b9376120a382e21212263
naikajb <naika1492@gmail.com>
2023-10-13 16:52:18 -0400

    .

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import logo from './logo.png';
-import navIcon from './navigation-bar.png';
+import navIcon from './nav.png';
 function Navbar(){
 
     return (
@@ -8,17 +8,18 @@ function Navbar(){
             <div>
             <img class = "Navbar-logo" src = {logo} alt = "logo" width = "100%" height = "100%"/> 
             </div>
-            <p className='companyName'> 
-                <h1>Company Name</h1>
-            </p>    
-            <ul>
-                <li className='Navlinks'>Home</li>
-                <li className='Navlinks'>Menu</li>
-                <li className='Navlinks'>Order</li>
-                <li className='Navlinks'>Contact</li>
-                <img class = "nav-icon" src = {navIcon} alt = "logo" width = "10%" height = "50%"/>
-                
-            </ul>
+                <p className='companyName'> 
+                    <h1>Company Name</h1>
+                </p>    
+                <ul>
+                    <li className='Navlinks'>Home</li>
+                    <li className='Navlinks'>Menu</li>
+                    <li className='Navlinks'>Order</li>
+                    <li className='Navlinks'>Contact</li>
+                    <a href = " ">
+                        <img class = "nav-icon" src = {navIcon} alt = "logo" width = "20%" height = "20%"/>
+                    </a>
+                </ul>
         </nav>
         
     )
