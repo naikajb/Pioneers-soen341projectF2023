5eaba9d26bea40db4cdad84fa3e51be128409424
Naika JB <jbaptiste.naika@gmail.com>
2023-10-17 00:17:29 -0400

    .

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Components/Navbar.css';
+import Navbar from './Components/Navbar.js';
+
+function App() {
+  return (
+    <div class = "Navbar">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App;
+
