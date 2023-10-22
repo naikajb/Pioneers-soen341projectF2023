0bc719a46f89b0e412e96858fa258332e74f0b5c
Manreet Kaur Lotay <99095521+manreetlotay@users.noreply.github.com>
2023-10-21 22:24:44 -0400

    Add CardGrid to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Components/Navbar.css';
 import Navbar from './Components/Navbar.js';
+import CardGrid from "./Components/CardGrid";
+import './App.css';
 
 function App() {
   return (
     <div class = "Navbar">
       <Navbar/>
+    <CardGrid />
     </div>
   )
 }
 
 export default App;
+
+
 
