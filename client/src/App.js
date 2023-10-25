925a7a05b74f8cc312fec46b4eb2eb5a7702cd88
unknown <lotaymk@gmail.com>
2023-10-25 13:22:52 -0400

    Add routes to different pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './Components/Navbar.css';
 import Navbar from './Components/Navbar.js';
 import CardGrid from "./Components/CardGrid";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App-container">
-      <Navbar />
-      <CardGrid />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Navbar />} />
+        <Route path = '/grid' element={<CardGrid />} />
+        </Routes>
+    </Router>
+    
   )
 }
 
