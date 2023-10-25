6d840e5422657cfc99d10eaaf277750223375a21
unknown <lotaymk@gmail.com>
2023-10-25 00:33:10 -0400

    Add id for each CardItem

--- client/src/Components/CardGrid.js
+++ client/src/Components/CardGrid.js
@@ -7,6 +7,7 @@ import "../App.css"
 
 const cardData = [
   {
+    id: 1,
     image: img,
     price: "$599,999",
     address: "1455 Blvd. De Maisonneuve Ouest",
@@ -14,6 +15,7 @@ const cardData = [
     bathroom: 2
   },
   {
+    id: 2,
     image: img,
     price: "$1,045,657",
     address: "1455 Blvd. De Maisonneuve Ouest",
@@ -21,6 +23,7 @@ const cardData = [
     bathroom: 3
   },
   {
+    id: 3,
     image: img,
     price: "$234,567",
     address: "1455 Blvd. De Maisonneuve Ouest",
@@ -28,6 +31,7 @@ const cardData = [
     bathroom: 2
   },
   {
+    id: 4,
     image: img,
     price: "$780,500",
     address: "1455 Blvd. De Maisonneuve Ouest",
@@ -35,6 +39,7 @@ const cardData = [
     bathroom: 3
   },
   {
+    id: 5,
     image: img,
     price: "$450,000",
     address: "1455 Blvd. De Maisonneuve Ouest",
@@ -42,6 +47,7 @@ const cardData = [
     bathroom: 2
   },
   {
+    id: 6,
     image: img,
     price: "$850,000",
     address: "1455 Blvd. De Maisonneuve Ouest",
