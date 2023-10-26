9e99f40fe86a0f7bc4356d9dc74fd26c29104d63
unknown <lotaymk@gmail.com>
2023-10-26 13:09:55 -0400

    mc

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.get('/listing/:id', (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
 });
