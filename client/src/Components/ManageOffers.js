cd7dd0772dbfa3f741bfbd416fa4294c9683e7b1
Naika JB <jbaptiste.naika@gmail.com>
2023-11-26 21:12:23 -0500

    managing offers works w/dummy data

--- client/src/Components/ManageOffers.js
+++ client/src/Components/ManageOffers.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { Button, Form } from "react-bootstrap";
 import { v4 as uuid } from "uuid";
 import { Link, useNavigate } from "react-router-dom";
+import { UserContext } from "../context/userContext.js";
 
 {/*import "bootstrap/dist/css/bootstrap.min.css";*/ }
 
@@ -22,6 +23,51 @@ const dummyOffers = [
         },
         image: "",
     },
+    status:"Pending",
+    //"broker": ObjectId,
+    //"_id": ObjectId,
+    FirstName: "John",
+    LastName: "Doe",
+    email: "test@mail.com",
+  },
+  {
+    id: 2,
+    price:100000,
+    property: {
+      price: 90000, 
+        address: "1234 main street",
+        bedroom: 3,
+        bathroom: 3,
+        amenities: ["pool", "gym", "parking"],
+        broker: { 
+            name: "Test", 
+            contact: "test@broker",
+        },
+        image: "",
+    },
+    //"broker": ObjectId,
+    //"_id": ObjectId,
+    status:"Pending",
+    FirstName: "Joh",
+    LastName: "Doe",
+    email: "jdoe@mai.com",
+  },
+  {
+    id: 3,
+    price:100000,
+    property: {
+      price: 90000, 
+        address: "1234 main street",
+        bedroom: 3,
+        bathroom: 3,
+        amenities: ["pool", "gym", "parking"],
+        broker: { 
+            name: "Test Broker", 
+            contact: "test@broker",
+        },
+        image: "",
+    },
+    status:"Pending",
     //"broker": ObjectId,
     //"_id": ObjectId,
     FirstName: "John",
@@ -29,6 +75,28 @@ const dummyOffers = [
     email: "test@mail.com",
   },
 
+  {
+    id: 4,
+    price:10000,
+    property: {
+      price: 90000, 
+        address: "1234 main street",
+        bedroom: 3,
+        bathroom: 3,
+        amenities: ["pool", "gym", "parking"],
+        broker: { 
+            name: "Test Broker", 
+            contact: "test@broker",
+        },
+        image: "",
+    },
+    status:"Pending",
+    //"broker": ObjectId,
+    //"_id": ObjectId,
+    FirstName: "John",
+    LastName: "Doe",
+    email: "test@mail.com",
+  },
     // {id: 2,
     // property: "1234 Main St",
     // offer: 1000,
@@ -69,31 +137,38 @@ const contactBroker = (buyer, offer) => {
   const mailtoLink = `mailto:${brokerEmail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
   window.open(mailtoLink);
 }
-function ManageOffers() {
-    const [offers, setOffers] = useState(dummyOffers);
-    // const [loading, setLoading] = useState(true);
 
-    // useEffect(() => {
-    //     setLoading(true);
-    //     axios.get('http://localhost:5000/offers')
-    //         .then((res) => {
-    //             setOffers(res.data);
-    //             setLoading(false);
-    //         })
-    //         .catch((err) => {
-    //             console.log(err);
-    //         });
-    // }, []);
+function ManageOffers() {
+
+  const [offers, setOffers] = useState(dummyOffers);
+  const  user = {name: "Test Broker"};
+
+  const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get('http://localhost:5000/offers')
+            .then((res) => {
+                setOffers(res.data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
     // if (loading) {
     //     return <p>Loading offers...</p>;
     // }
     const groupedOffers = {};
     offers.forEach((offer) => {
-        if (!groupedOffers[offer.property]) {
+      if(offer.property.broker.name === user.name){ //only show offers for properties that logged in broker owns
+        //groups them by property
+        if (!groupedOffers[offer.property]) { 
             groupedOffers[offer.property] = [];
         }
         groupedOffers[offer.property].push(offer);
+      }
     });
 
     // const [offers, setOffers] = useState();
@@ -114,7 +189,7 @@ function ManageOffers() {
                     offer.status = "Rejected";
                 }
             }else{
-              if(action === "accept" && offer.property === selectedOffer.property){
+              if(action === "accept" && offer.property.address === selectedOffer.property.address){
                 offer.status = "Rejected";
               }
             }
@@ -128,13 +203,13 @@ function ManageOffers() {
             <h1 className="title-offers-page">Manage Offers</h1>
             {Object.entries(groupedOffers).map(([property, offers]) => (
                 <div className = "offers" key = {property}>
-                <h2>{property}</h2>
                 <ul>
                     {offers.map((offer,index) => (
                         <li className = "offer-detail" key = {index}>
-                          <p><b>Amount: </b>{offer.offer}$</p>
-                          <p><b>Buyer: </b>{offer.user}</p>
-                          <p><b>Status: </b>{offer.status}</p>
+                          <p><b>Property: </b>{offer.property.address}</p>
+                          <p><b>Amount: </b>{offer.price}$</p>
+                          <p><b>Buyer: </b>{offer.LastName}, {offer.FirstName}</p>
+                          <p><b>Status: </b>pending</p>
                           {offer.status === "Pending" ? (
                             <div>
                               <button className="contact-butt" onClick={() => contactBroker(offer.buyer, offer)}>Contact Broker</button>
